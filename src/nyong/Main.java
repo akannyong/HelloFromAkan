2b6d2e20f173f28fa31f1d99b85f4e66cb28a3ff
akan nyong <akanefioknyong@gmail.com>
2021-10-12 12:01:59 +0100

    Initial commit

---
+++ src/nyong/Main.java
@@ -0,0 +1,15 @@
+package nyong;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Main main = new Main();
+        main.Hello();
+    }
+
+    public void Hello() {
+        System.out.println();
+        System.out.println("Hello, my name is Akan");
+    }
+}
